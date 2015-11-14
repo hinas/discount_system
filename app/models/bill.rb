3af10b7fd33bc9638673abe199a46773e6c1339f
hinas <hs_250193@yahoo.co.in>
2015-11-14 16:49:42 +0530

    indentation

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,7 +1,7 @@
 class Bill < ActiveRecord::Base
-	attr_accessor :user_id, :amount, :order_time, :discount_code, :mode_of_payment, :category_id
+  attr_accessor :user_id, :amount, :order_time, :discount_code, :mode_of_payment, :category_id
 
-	def initialize(user_id, amount, order_time, discount_code, mode_of_payment, category_id)
+  def initialize(user_id, amount, order_time, discount_code, mode_of_payment, category_id)
     @user_id = user_id
     @amount = amount
     @order_time = order_time
